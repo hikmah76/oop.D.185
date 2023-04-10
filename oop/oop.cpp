9fa09cad097436eeb8a77ac7446dd9805a736154
nur hikmah <nur.hikmah190504@gmail.com>
2023-04-10 14:42:54 +0700

    Add project files.

---
+++ oop/oop.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    int nim;
+    string nama;
+    void tampil() {
+        cout << "NIM= " << nim;
+        cout << "\nNama= " << nama;
+    }
+};
+
+class Matakuliah {
+
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
